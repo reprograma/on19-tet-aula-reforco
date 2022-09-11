67a4d3f6b872b3a7eaeb0f12088a8d2f60df479b
mflilian <mayhharamorais@gmail.com>
2022-09-11 01:08:30 -0300

    add exercise escopo

---
+++ exercicios/logica2/escopo/escopo.js
@@ -0,0 +1,11 @@
+// o que há de errado com esse trecho de código? justifique
+
+function ola() {
+    function mensagem() {
+        return 'Olá'
+    }
+    console.log(mensagem())
+}
+ola()
+
+console.log(mensagem()) 
